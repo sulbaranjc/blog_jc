d57f32ddbe39a403d4658cb9e4702987c45aee00
Juan Carlos <Sulbaranjc@gmail.com>
2020-03-22 12:04:55 -0300

    App HTML CON TR ok

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,33 @@
 import React from "react";
 
 const App = () => {
-  return <div>Hola Mundo JC</div>;
+  const ponerFilas = () => [
+    <tr>
+      <td>Rodolfo</td>
+      <td>Rodolfo@platzi.com</td>
+      <td>Rodolfo.com</td>
+    </tr>,
+    <tr>
+      <td>Rodolfo</td>
+      <td>Rodolfo@platzi.com</td>
+      <td>Rodolfo.com</td>
+    </tr>
+  ];
+
+  return (
+    <div className="margen">
+      <table className="tabla">
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Correo</th>
+            <th>Enlace</th>
+          </tr>
+        </thead>
+        <tbody>{ponerFilas()}</tbody>
+      </table>
+    </div>
+  );
 };
+
 export default App;
